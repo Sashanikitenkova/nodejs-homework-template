5cf98cbc60cf73e4539084871fe9a114f17d633d
Sashanikitenkova <81653543+Sashanikitenkova@users.noreply.github.com>
2022-11-30 23:35:56 +0100

    1

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,5 +11,6 @@ router.post("/login", validation(joiSchema), ctrlWrapper(ctrl.login));
 router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 router.get("/logout", auth, ctrlWrapper(ctrl.logout));
 router.patch("/avatars", auth, upload.single("avatar"), ctrlWrapper(ctrl.updateAvatar));
+router.get("/verify/:verificationToken", ctrlWrapper(ctrl.verifyEmail));
 
 module.exports = router;
